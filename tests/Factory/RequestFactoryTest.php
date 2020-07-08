b0c24d5d39e9de52bde2ccf4ffb5584287b3f3a1
RasulArslanov <rasularslanoov@gmail.com>
2020-07-08 21:26:38 +0500

    Added Request Factory (Implementing PSR-17)

---
+++ tests/Factory/RequestFactoryTest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace tests\Arslanoov\Psr7\Factory;
+
+use Arslanoov\Psr7\Factory\RequestFactory;
+use Arslanoov\Psr7\Request;
+use Arslanoov\Psr7\Uri;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\RequestInterface;
+
+class RequestFactoryTest extends TestCase
+{
+    public function testSuccess(): void
+    {
+        $request = (new RequestFactory())->createRequest($method = 'GET', $uri = new Uri('/home'));
+
+        $this->assertInstanceOf(Request::class, $request);
+        $this->assertInstanceOf(RequestInterface::class, $request);
+        $this->assertSame($request->getMethod(), $method);
+        $this->assertSame($request->getUri(), $uri);
+    }
+}
